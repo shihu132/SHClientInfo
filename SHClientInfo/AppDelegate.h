12ac5050bad763cfa995fdcd81b5a8b5fc71aad9
shihu <13261089216@163.com>
2018-11-20 21:51:17 +0800

    Initial Commit

---
+++ SHClientInfo/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  SHClientInfo
+//
+//  Created by joyshow on 2018/11/20.
+//  Copyright © 2018年 石虎. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
